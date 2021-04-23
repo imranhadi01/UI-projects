51b0fa42133eabe239e2da337c7aab9458b521dd
imran <mijanjua1212@gmail.com>
2021-04-23 11:48:16 +0500

    first commit

---
+++ login page/dom.js
@@ -0,0 +1,26 @@
+var button = document.getElementById('btn');
+
+var para = document.getElementById('para');
+
+
+button.addEventListener('click', funn);
+function funn(){
+    for(var i = 0; i < 10; i++){
+        para[i] 
+    }
+
+
+
+
+}
+
+function coountdown(){
+    var currentval = document.getElementById('countdown').innerHTML;
+    var newval = currentval - 1 ;
+    document.getElementById('countdown').innerHTML = newval; 
+
+
+}
+
+
+
